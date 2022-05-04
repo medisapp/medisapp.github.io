7385dc8a70f5a7dbaad5d0974a673b3c71a25780
Zihua Li <i@zihua.li>
2022-05-04 21:54:05 +0800

    Production

--- docs/pricing.982ab827.js
+++
@@ -1,2 +0,0 @@
-Paddle.Setup({vendor:Number("5932")}),Paddle.Environment.set("sandbox"),window.PRODUCT_ID=Number("28229");
-//# sourceMappingURL=pricing.982ab827.js.map
---
+++ docs/pricing.c06818eb.js
@@ -0,0 +1,2 @@
+Paddle.Setup({vendor:Number("146577")}),window.PRODUCT_ID=Number("768770");
+//# sourceMappingURL=pricing.c06818eb.js.map
